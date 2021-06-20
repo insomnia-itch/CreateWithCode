70b7006127a2d776b24adf4bd0ae9bba7cec2ce9
Jelani Woods <jelani@firstdraft.com>
2021-06-20 14:18:33 -0500

    Add ability to toggle camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Cameras")]
+    public Camera firstPersonCamera;
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPersonCamera.enabled = false;
+        mainCamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           toggleCamera();
+        }
+    }
+
+    public void toggleCamera()
+    {
+        firstPersonCamera.enabled = !firstPersonCamera.enabled;
+        mainCamera.enabled = !mainCamera.enabled;
+    }
+}
